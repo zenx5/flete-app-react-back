2579121f27c6b08576854ff43bd279c2cb135f74
Octavio Martinez <omartinez1618@gmail.com>
2023-05-02 15:08:13 -0400

    feat(auction): add start and end coordinates to Auction model feat(auction): add auctionMiddleware to calculate current, last and original price refactor(auction): update getAllAuctions to include bids in response

--- controllers/AuctionController.js
+++ controllers/AuctionController.js
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import auctionMiddleware from "../middleware/auctionMiddleware.js";
 import { getAll, getById } from "./BaseController.js";
 
 const prisma = new PrismaClient()
+prisma.$use(auctionMiddleware)
 const entity = prisma.auction
 
 export const getAllAuctions = getAll( entity, { bids:true })
---
+++ middleware/auctionMiddleware.js
@@ -0,0 +1,29 @@
+export default async function auctionMiddleware(params, next) {
+    console.log(params.model, params.action)
+    if( params.model==='Auction') {
+        const result = await next(params)
+        if( params.action === 'findMany' ) {
+            return result.map( item => ({
+                ...item,
+                current_price: calculatePrice(item.price, item.bids),
+                last_price: calculatePrice(item.price, item.bids.slice(0,-1)),
+                original_price: item.price
+            }))
+        } else {
+            return {
+                ...result,
+                current_price: calculatePrice(result.price, result.bids),
+                last_price: calculatePrice(result.price, result.bids.slice(0,-1)),
+                original_price: result.price
+            }
+        }
+    }
+}
+
+const calculatePrice = (price, bids) => {
+    let total_bids = 0
+    for( const bid of bids ) {
+        total_bids += bid.value
+    }
+    return price - total_bids;
+}
